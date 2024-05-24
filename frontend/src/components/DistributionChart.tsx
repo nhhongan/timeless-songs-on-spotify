684727e6423213da9bb9cff00a4bacfba8f63210
Phu Chau <chauanphu@gmail.com>
2024-05-24 22:04:01 +0700

    style: align grid middle

--- frontend/src/components/DistributionChart.tsx
+++ frontend/src/components/DistributionChart.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { max, scaleLinear, select, axisBottom, axisLeft, bin, extent } from "d3";
 import { fetchSongFeatures } from "../utils/api";
-import "../styles/Distribution.scss";
+import "../styles/Distribution.scss"; 
 interface BarChartProps {}
 
 interface HistogramProps {
